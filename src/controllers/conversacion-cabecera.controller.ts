668e6ad039cbabbc9366705ea080f8dc38d84b58
DiegoB <diegobaesv@gmail.com>
2024-10-27 19:23:17 -0500

    cambios api

--- src/controllers/conversacion-cabecera.controller.ts
+++ src/controllers/conversacion-cabecera.controller.ts
@@ -4,8 +4,12 @@ import * as conversacionCabeceraService from "../services/conversacion-cabecera.
 export const obtenerUltimaConversacionCabecera = async (request: Request, response: Response) => {
     try {
         const { telefono } = request.params;
-        const result = await conversacionCabeceraService.obtenerUltimaConversacionCabecera(telefono);
-        response.json({success: true, data: result, message: 'OK'});
+        let result: any[] = await conversacionCabeceraService.obtenerUltimaConversacionCabecera(telefono);
+        if(result.length == 0) {
+            await conversacionCabeceraService.insertarConversacionCabecera(telefono, '20241020223915');
+            result = await conversacionCabeceraService.obtenerUltimaConversacionCabecera(telefono);
+        }
+        response.json({success: true, data: result[0], message: 'OK'});
     } catch (error) {
         response.json({success: false, data: null, message: error});
     }
--- src/services/conversacion-cabecera.service.ts
+++ src/services/conversacion-cabecera.service.ts
@@ -1,7 +1,7 @@
 import pool from "../config/db"
 
 export const obtenerUltimaConversacionCabecera = async (telefono: string) => {
-    console.log('conversacionService::insertarConversacion');
+    console.log('conversacionService::obtenerUltimaConversacionCabecera');
     const result = await pool.query(`select cc.id_conversacion_cabecera , cc.id_cliente, cc.telefono, cc.hash, cc.cod_pedido, c.id_estado ultimo_estado
                     from bot_conversacion_cabeceras cc
                     left join bot_conversaciones c on c.id_conversacion_cabecera = cc.id_conversacion_cabecera
@@ -9,6 +9,13 @@ export const obtenerUltimaConversacionCabecera = async (telefono: string) => {
                     order by cc.fecha_creacion_auditoria desc, c.fecha_creacion_auditoria desc
                     limit 1`, [telefono]);
 
-    console.log('result',result);
     return result.rows;
+}
+
+export const insertarConversacionCabecera = async (telefono: string, hash: string) => {
+    console.log('conversacionService::insertarConversacionCabecera');
+    await pool.query(
+        'INSERT INTO bot_conversacion_cabeceras (telefono, hash) VALUES ($1, $2)',
+        [telefono, hash]
+    );
 }
